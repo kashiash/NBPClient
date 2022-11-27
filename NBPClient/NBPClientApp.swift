0978583033c3a08a7aa5fc2f4ee5fb07c4f6c38a
Jacek Kosinski <kashiash@gmail.com>
2022-11-27 18:05:17 +0100

    Initial Commit

---
+++ NBPClient/NBPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBPClientApp.swift
+//  NBPClient
+//
+//  Created by Jacek Kosiński G on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
